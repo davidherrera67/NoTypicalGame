4854db9dfeff9d879df71ae1a92afec6d11c4b27
David <herreracostadavid@gmail.com>
2022-05-12 22:27:06 +0200

    Changes

---
+++ core/src/main/java/com/game/Actors/ActorSpike.java
@@ -0,0 +1,27 @@
+package com.game.Actors;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class ActorSpike extends Actor {
+
+    private TextureRegion spikes;
+
+    public ActorSpike(TextureRegion spikes) {
+        this.spikes = spikes;
+        setSize(spikes.getRegionWidth(),spikes.getRegionHeight());
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(spikes,getX(),getY());
+
+    }
+
+    @Override
+    public void act(float delta) {
+        setX(getX()-250*delta);
+    }
+}
--- core/src/main/java/com/game/MainGameScreen.java
+++ core/src/main/java/com/game/MainGameScreen.java
@@ -2,21 +2,36 @@ package com.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.game.Actors.ActorPlayer;
+import com.game.Actors.ActorSpike;
 
-public class MainGameScreen extends BaseScreen{
+public class MainGameScreen extends BaseScreen {
     private Stage stage;
     private ActorPlayer actorPlayer;
+    private ActorSpike actorSpike;
+    private Texture playerTexture;
+    private Texture spikesTexture;
+    private TextureRegion spikesRegion;
 
     public MainGameScreen(Game game) {
         super(game);
+        playerTexture = new Texture("DinoReescalado.png");
+        spikesTexture = new Texture("pinchoReescalado.png");
+        spikesRegion = new TextureRegion(spikesTexture);
     }
 
     @Override
     public void show() {
         stage = new Stage();
-        actorPlayer = new ActorPlayer();
+        actorPlayer = new ActorPlayer(playerTexture);
+        actorSpike = new ActorSpike(spikesRegion);
         stage.addActor(actorPlayer);
+        stage.addActor(actorSpike);
+        actorPlayer.setPosition(20, 100);
+        actorSpike.setPosition(800, 100);
         //darle posicion al jugador
         //actorPlayer.setPosition(0,0);
     }
@@ -27,8 +42,17 @@ public class MainGameScreen extends BaseScreen{
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         //pide que se actualicen
         stage.act();
+        checkColisions();
         //se pinta toodo el escenario de golpe
         stage.draw();
+    }
+
+
+    private void checkColisions() {
+        if (actorPlayer.isAlive() && (actorPlayer.getX() + actorPlayer.getWidth()) > actorSpike.getX()) {
+            System.out.println("FIN DE LA PARTIDA");
+            actorPlayer.setAlive(false);
+        }
     }
 
     @Override
@@ -37,4 +61,9 @@ public class MainGameScreen extends BaseScreen{
         stage.dispose();
     }
 
+    @Override
+    public void dispose() {
+        playerTexture.dispose();
+        spikesTexture.dispose();
+    }
 }
